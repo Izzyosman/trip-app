9431593a190bcce9097031c67126a3e588fde836
Izzyosman <137222951+Izzyosman@users.noreply.github.com>
2023-08-10 10:27:25 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const controller = require('./controller');
-const { sequelize } = require('./database/models');
+const controller = require('./controller.js');
+const { sequelize } = require('./database/models.js');
 
 const app = express();
 const PORT = 3000;
@@ -12,6 +12,20 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/style.css', (req, res) => {
+  res.setHeader('Content-Type', 'text/css');
+  res.sendFile(path.join(__dirname, 'style.css'));
+});
+
+app.get('/script.js', (req, res) => {
+  res.setHeader('Content-Type', 'application/javascript');
+  res.sendFile(path.join(__dirname, 'script.js'));
+});
+
+app.get('/tropical.png', (req, res) => {
+  res.sendFile(path.join(__dirname, 'tropical.png'));
 });
 
 app.get('/about', (req, res) => {
