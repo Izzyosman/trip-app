bdede1aa8c4a41beee105b4845c0c3e1a10f8a84
Izzyosman <137222951+Izzyosman@users.noreply.github.com>
2023-08-23 22:53:57 -0500

    first commit

--- server.js
+++ server.js
@@ -1,57 +1,39 @@
 require('dotenv').config();
 const cors = require('cors'); 
-const {SERVER_PORT} = process.env;
+const PORT = process.env.SERVER_PORT || 5432;
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const controller = require('./controller.js');
-const { sequelize } = require('./databases/models.js');
 
-const {seed, addDestination, updateDestination, deleteDestination, getDestinations} = require('./controller.js')
+const {
+  seed,
+  addCountry,
+  updateCountry,
+  deleteCountry,
+  getCountry,
+} = require('./controller.js');
 
 const app = express();
-const PORT = 3306;
 
 app.use(cors()); 
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
+// Static files
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+app.get('/style.css', (req, res) => res.sendFile(path.join(__dirname, 'style.css')));
+app.get('/script.js', (req, res) => res.sendFile(path.join(__dirname, 'script.js')));
+app.get('/tropical.png', (req, res) => res.sendFile(path.join(__dirname, 'tropical.png')));
 
-app.get('/style.css', (req, res) => {
-  res.setHeader('Content-Type', 'text/css');
-  res.sendFile(path.join(__dirname, 'style.css'));
-});
+// API routes
+app.post('/api/vacation/:id', addCountry);
+app.get('/api/vacation/:id', getCountry);
+app.put('/api/vacation/:id', updateCountry);
+app.delete('/api/vacation/:id', deleteCountry);
 
-app.get('/script.js', (req, res) => {
-  res.setHeader('Content-Type', 'application/javascript');
-  res.sendFile(path.join(__dirname, 'script.js'));
-});
+// DEV
+app.post('/seed', seed);
 
-app.get('/tropical.png', (req, res) => {
-  res.sendFile(path.join(__dirname, 'tropical.png'));
-});
-
-app.get('/about', (req, res) => {
-    res.sendFile(path.join(__dirname, 'about.html'));
-});
-
-app.post('/api/vacation', controller.addDestination);
-app.put('/api/vacation/:id', controller.updateDestination);
-app.delete('/api/vacation/:id', controller.deleteDestination);
-app.get('/api/vacation', controller.getDestinations);
-
-app.listen(PORT, async () => {
-    console.log(`Server running on PORT:${SERVER_PORT}/`);
-
-    try {
-    await sequelize.authenticate();
-    console.log('Db connected');
-    await sequelize.sync({ alter: true });
-    } catch (error) {
-      console.error('Error connecting to the database:', error);
-    }
+app.listen(PORT, () => {
+    console.log(`Server running on PORT:${PORT}/`);
 });
